39c11d52627f5a436ad87e69e15a93db6faef1b4
01452 <andreyshahnovichtelran@gmail.com>
2025-05-25 20:08:36 +0200

    added auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as process from 'node:process';
+import { User, UserSchema } from './users/model/user.schema';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`${process.env.MONGO_URL}`),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
